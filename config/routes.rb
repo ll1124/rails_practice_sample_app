134d2934a511b5f432215921483fa9d4569f8915
jin <ll1124@hanmail.net>
2017-03-30 17:19:33 +0900

    before 4.3.4

--- config/routes.rb
+++ config/routes.rb
@@ -9,3 +9,13 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 end
+
+
+(1..5).each do |number|
+  p 2 * number
+  puts '------------'
+end
+
+a = %w[abc a c d e f dsa gasd babf fdbds].map { |e| e.upcase  }
+
+p a.map(&:downcase).sort.reverse
