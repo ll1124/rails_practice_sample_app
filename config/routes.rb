fdfa6cf873d4fc9ce73edc42f6814601519a0a2d
jin <ll1124@hanmail.net>
2017-05-07 15:57:45 +0900

    chapter 5 finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/home'
+  get '/signup', to: 'users#new'
 
-  get 'static_pages/help'
+  get '/home', to: 'static_pages#home'
 
-  get 'static_pages/about'
+  get '/help', to: 'static_pages#help'
 
-  get 'static_pages/contact'
+  get '/about', to: 'static_pages#about'
+
+  get '/contact', to: 'static_pages#contact'
 end
-
-
-(1..5).each do |number|
-  p 2 * number
-  puts '------------'
-end
-
-a = %w[abc a c d e f dsa gasd babf fdbds].map { |e| e.upcase  }
-
-p a.map(&:downcase).sort.reverse
